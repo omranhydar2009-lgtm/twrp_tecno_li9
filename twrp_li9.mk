b3b8b2d70bcd347704deab6507e2acfb663c9089
omranhydar2009-lgtm <omranhydar2009@gmail.com>
2026-03-19 21:21:55 +0300

    Update twrp_li9.mk

--- twrp_li9.mk
+++ twrp_li9.mk
@@ -1,6 +1,20 @@
+# Inherit from generic 64-bit product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from TWRP configuration (Essential)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier
 PRODUCT_DEVICE := li9
 PRODUCT_NAME := twrp_li9
 PRODUCT_BRAND := Tecno
 PRODUCT_MODEL := Pova 6 Pro 5G
 PRODUCT_MANUFACTURER := tecno
+
+# Platform version for Android 15
+PLATFORM_VERSION := 15
+PLATFORM_SDK_VERSION := 35
+
+# Fingerprint (Optional but helpful for decryption)
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
